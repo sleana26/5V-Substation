4336c6daf9dd2622f60a49240fccd66fc5e4ad90
sleana26 <sleana26@gmail.com>
2025-08-12 22:09:33 -0400

    partially implemented local recloser authentication

--- src/Recloser/authentication.c
+++ src/Recloser/authentication.c
@@ -3,9 +3,54 @@
  * 
  */
 #include <stdio>
+#include "argon2.h"
+#include <stdlib>
 
-string user = admin;
-string pass = admin;
+#define MAX_PASS_LEN = 16
+#define HASH_LEN = 32
+#define SALT_LEN = 16
+
+readPass() {
+    
+    //open password file to see if admin account already has a password set
+    FILE *passfptf;
+    passfptf = fopen("_____", "r");
+    if(fptr == NULL) {
+        printf("Error: Can't access password(s).\n");
+        return 1;
+    }
+    //check if password file contains a password in password file
+    fscanf(passfptf, "%c", &password)
+
+    fscanf(passfptf, "%c", &password)
+    while()
+
+    fclose(passfptf)
+
+    //if password is needed
+    //Initialize password array
+    char pass[MAX_PASS_LEN] = "";
+    printf("Set password for admin account: ")
+    while()
+    pass[i] = getchar();
+
+    
+    //if password file is empty
+    if()
+    printf("Set password for admin account: ")
+    
+    printf("Please")
+}
+
+
+for(i = 0; i < MAX_PASS_LEN; i++) {
+    pass[i] = 
+}
+
+
+hashpass() {
+
+}
 
 /*
  * The program will add salt and hash the password using argon2. Password will be stored in a text or SQLite file
